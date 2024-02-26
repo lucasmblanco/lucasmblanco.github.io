4ee6cb41085dc179b333394ad6a0b51993255042
Lucas Matias Blanco <lucasmatiasblanco@outlook.com>
2024-02-26 01:59:29 -0300

    More fixes in tech.ts

--- src/content/es/tech.ts
+++ src/content/es/tech.ts
@@ -1,7 +1,1 @@
-export const p1 = ` Voy experimentando con diferentes tecnologias para poder tener un abanico
-cada vez más amplio de posibles soluciones a la hora de abordar un problema;
-sin importar si es relacionado al front o al back, considero que es
-importante aprender el funcionamiento de las tecnologías desde ambas
-perspectivas para así lograr que su aplicación sea más óptima. Esto también
-me permite tener flexibilidad para incorporar rapidamente nuevos
-conocimientos.`
+export const p1 = `Voy experimentando con diferentes tecnologías para poder tener un abanico cada vez más amplio de posibles soluciones a la hora de abordar un problema; sin importar si es relacionado al front o al back, considero que es importante aprender el funcionamiento de las tecnologías desde ambas perspectivas para así lograr que su aplicación sea más óptima. Esto también me permite tener flexibilidad para incorporar rápidamente nuevos conocimientos.`
